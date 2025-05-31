1ca61105cca1be3fa42d46af3f5ca6b6f06c0ebf
Nstaya <koshelevaa06@mail.ru>
2025-05-31 18:01:26 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -4,15 +4,9 @@
 #include <iostream>
 
 int main() {
-
-    try {
-        registerShapeCreators();
-        ShapeStorage storage;
-        Menu menu(storage);
-        menu.run();
-    } catch (const std::exception& e) {
-        std::cerr << "Ошибка: " << e.what() << std::endl;
-        return 1;
-    }
+    registerShapeCreators();
+    ShapeStorage storage;
+    Menu menu(storage);
+    menu.run();
     return 0;
 }
